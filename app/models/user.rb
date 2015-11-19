80cb56662fa79e9d880ac2aa15a17065f5bbf049
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-19 16:04:17 +0000

    Add payment model. Update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
