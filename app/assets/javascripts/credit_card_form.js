2209ff7463cb5866f086922b4017a21b04e57f9c
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-23 15:32:14 +0000

    Fix problem with Turbolinks.

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,51 +1,54 @@
-$(document).ready(function () {
-    function submitHandler(event) {
-        var $form = $(event.target);
+$(document).on('page:change', start);
+$(document).ready(start);
 
-        $form.find("input[type=submit]").prop("disabled", true);
-
-        // If Stripe was initialized correctly this will create a token using the credit card info
-        if(Stripe) {
-            Stripe.card.createToken($form, stripeResponseHandler);
-        }
-        else {
-            show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
-        }
-
-        return false;
-    };
-
-    function stripeResponseHandler(status, response) {
-        var token, $form = $('.cc_form');
-
-        if(response.error) {
-            console.log(response.error.message);
-            show_error(response.error.message);
-            $form.find("input[type=submit]").prop("disabled", false);
-        } else {
-            token = response.id;
-            $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
-            $("[data-stripe=number]").remove();
-            $("[data-stripe=cvc]").remove();
-            $("[data-stripe=exp-year]").remove();
-            $("[data-stripe=exp-month]").remove();
-            $("[data-stripe=label]").remove();
-            $form.get(0).submit();
-        }
-
-        return false;
-    };
-
-    function show_error(message) {
-        if($("#flash-messages").size() < 1) {
-            $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
-        }
-
-        $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
-        $('.alert').delay(5000).fadeOut(3000);
-
-        return false;
-    };
-
+function start() {
     $(".cc_form").on('submit', submitHandler);
-});
+}
+
+function submitHandler(event) {
+    var $form = $(event.target);
+
+    $form.find("input[type=submit]").prop("disabled", true);
+
+    // If Stripe was initialized correctly this will create a token using the credit card info
+    if(Stripe) {
+        Stripe.card.createToken($form, stripeResponseHandler);
+    }
+    else {
+        show_error("Failed to load credit card processing functionality. Please reload this page in your browser.")
+    }
+
+    return false;
+}
+
+function stripeResponseHandler(status, response) {
+    var token, $form = $('.cc_form');
+
+    if(response.error) {
+        console.log(response.error.message);
+        show_error(response.error.message);
+        $form.find("input[type=submit]").prop("disabled", false);
+    } else {
+        token = response.id;
+        $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
+        $("[data-stripe=number]").remove();
+        $("[data-stripe=cvc]").remove();
+        $("[data-stripe=exp-year]").remove();
+        $("[data-stripe=exp-month]").remove();
+        $("[data-stripe=label]").remove();
+        $form.get(0).submit();
+    }
+
+    return false;
+}
+
+function show_error(message) {
+    if($("#flash-messages").size() < 1) {
+        $('div.container.main div:first').prepend("<div id='flash-messages'></div>")
+    }
+
+    $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
+    $('.alert').delay(5000).fadeOut(3000);
+
+    return false;
+}
