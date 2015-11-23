ae8f03774d8793bc60cf17c741cb5bb9716f7393
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-23 16:09:57 +0000

    Add initial image upload via Carrierwave
    Initial image/show.
    Ensure uploaded images are not in Git

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -25,6 +25,7 @@ class ImagesController < ApplicationController
   # POST /images.json
   def create
     @image = Image.new(image_params)
+    @image.user = current_user
 
     respond_to do |format|
       if @image.save
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :user
+
+  mount_uploader :picture, PictureUploader
 end
