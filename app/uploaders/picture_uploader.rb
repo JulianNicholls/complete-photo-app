0dbae54a10fc8b774c0c04ccf0b0da32bb38b22c
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-23 16:29:02 +0000

    Add validation and styling for images.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,6 +6,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  process resize_to_limit: [500, 500]
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -38,9 +40,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
